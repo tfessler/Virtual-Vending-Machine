4b3d6ffc98852103bedaa93dea040be26898f609
Tracy Fessler <tcfessler@gmail.com>
2021-06-04 09:12:30 -0400

    commit purchase and change classes

---
+++ java/src/main/java/com/techelevator/view/Purchase.java
@@ -0,0 +1,39 @@
+package com.techelevator.view;
+
+import java.util.HashMap;
+
+public class Purchase {
+
+
+    private double fedMoney;
+    private double productPrice;
+
+
+    public void Change (HashMap<Integer, Double> changeReturned, double fedMoney, double productPrice){
+        this.fedMoney = fedMoney;
+        this.productPrice = productPrice;
+    }
+    public double getFedDouble() {
+        return fedMoney;
+    }
+
+    public void setFedDouble(double fedDouble) {
+        this.fedMoney = fedDouble;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+
+
+
+
+
+
+
+}
